d47d91462312794a4e46ca788e68c1d5886ad556
znoxx <nzabrod@yandex.ru>
2022-05-27 21:11:21 +0000

    Fixing 3proxy config

--- dockerfiles/tbng-3proxy/entrypoint.sh
+++ dockerfiles/tbng-3proxy/entrypoint.sh
@@ -12,5 +12,7 @@ if [ "$AUTH" = 'AUTH' ]; then
   echo $PROXY_USERNAME:`/bin/mycrypt $$ $PROXY_PASSWORD` >> /conf/passwd
   CONFIGFILE=/conf/3proxy.auth
 fi
-
+touch /conf/nserver.conf
+ns=$(cat /etc/resolv.conf  | grep -v '^#' | grep nameserver | awk '{print $2}')
+echo "nserver $ns" >/conf/nserver.conf
 /bin/3proxy $CONFIGFILE
