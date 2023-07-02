6a4beeb3c584d23e77457f4d8e3e56db5352c220
znoxx <nzabrod@yandex.ru>
2023-07-02 14:21:43 +0300

    Moving to bookworm, switching to debian repo to install tor, fixing container names

--- get_bridge.sh
+++ get_bridge.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Bridge template..."
-docker exec bridge_tbng-bridge_1 cat /var/lib/tor/.tor/pt_state/obfs4_bridgeline.txt
+docker exec tbng-bridge cat /var/lib/tor/.tor/pt_state/obfs4_bridgeline.txt
 echo "Bridge fingerprint..."
-docker exec bridge_tbng-bridge_1 cat /var/lib/tor/.tor/fingerprint
+docker exec tbng-bridge cat /var/lib/tor/.tor/fingerprint
