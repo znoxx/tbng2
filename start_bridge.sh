ee976eac082d21debcf79090ef7d9f24edc44e2a
znoxx <nzabrod@yandex.ru>
2024-02-15 19:03:11 +0000

    Moving to separate compose parts

--- start_bridge.sh
+++ start_bridge.sh
@@ -3,5 +3,5 @@ set -e
 source $(pwd)/env_load.sh
 ./prepare_volumes.sh
 ./stop_bridge.sh
-docker-compose $ENVFILE -f compose/bridge/docker-compose.yaml up -d 
+docker compose $ENVFILE -f compose/bridge/docker-compose.yaml up -d 
 
--- stop_ap.sh
+++ stop_ap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 source $(pwd)/env_load.sh
-docker-compose $ENVFILE -f compose/tbng2/docker-compose.yaml down
+docker compose $ENVFILE -f compose/tbng2/docker-compose.yaml down
 
